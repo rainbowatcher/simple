06de2c95a13bb4ae9bed38eebfc7d9adeda29bd3
rainbowatcher <rainbow-w@qq.com>
2023-04-04 20:05:36 +0800

    feat: add 404 page content

--- src/pages/[...any].vue
+++ src/pages/[...any].vue
@@ -3,7 +3,11 @@
 </script>
 
 <template>
-  404
+  <NResult status="404" title="404 Resource don't exists" description="You have found a back hole" size="huge">
+    <template #footer>
+      <NButton>Back Home</NButton>
+    </template>
+  </NResult>
 </template>
 
 <style scoped>
