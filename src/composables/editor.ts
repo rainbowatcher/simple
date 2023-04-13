996296b0c17ae86d95f9996c70f8e94df8a5834c
rainbowatcher <rainbow-w@qq.com>
2023-04-13 09:22:20 +0800

    style: make monaco editor worker more readable

--- src/composables/editor.ts
+++ src/composables/editor.ts
@@ -7,20 +7,24 @@ import htmlWorker from "monaco-editor/esm/vs/language/html/html.worker?worker"
 import tsWorker from "monaco-editor/esm/vs/language/typescript/ts.worker?worker"
 
 self.MonacoEnvironment = {
-  getWorker(_: any, label: string) {
-    if (label === "json") {
-      return new jsonWorker()
+  getWorker(_workerId, label) {
+    switch (label) {
+      case "json":
+        return new jsonWorker()
+      case "css":
+      case "scss":
+      case "less":
+        return new cssWorker()
+      case "html":
+      case "handlebars":
+      case "razor":
+        return new htmlWorker()
+      case "typescript":
+      case "javascript":
+        return new tsWorker()
+      default:
+        return new editorWorker()
     }
-    if (label === "css" || label === "scss" || label === "less") {
-      return new cssWorker()
-    }
-    if (label === "html" || label === "handlebars" || label === "razor") {
-      return new htmlWorker()
-    }
-    if (label === "typescript" || label === "javascript") {
-      return new tsWorker()
-    }
-    return new editorWorker()
   },
 }
 
@@ -74,12 +78,12 @@ export const useMonacoEditor = (useOptions: EditorOptions) => {
     if (instance.value?.getModel()) {
       monaco.editor.setModelLanguage(instance.value.getModel()!, unref(lang))
     }
-    })
+  })
 
   const resize = () => {
     setTimeout(() => {
       instance.value?.layout()
-  })
+    })
   }
 
   return {
