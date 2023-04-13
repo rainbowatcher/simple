a331d3020a59b73d082e9709bfe1141ddc522929
rainbowatcher <rainbow-w@qq.com>
2023-04-13 09:23:26 +0800

    feat: init develop page

---
+++ src/components/app/Editor.vue
@@ -0,0 +1,28 @@
+<script lang="ts" setup>
+const props = withDefaults(defineProps<{
+  value?: string
+  language?: string
+  resize?: () => void
+}>(), {
+  value: "",
+  language: "handlebars",
+})
+const emit = defineEmits<{
+  (e: "update:value", val: string): void
+}>()
+const data = useVModel(props, "value", emit)
+const container = ref<HTMLDivElement>()
+const { instance, dispose, isLoading } = useMonacoEditor({ container, data })
+
+onMounted(() => {
+  dispose?.()
+})
+</script>
+
+<template>
+  <div ref="container" border="1 solid neutral100 dark:none" w-auto h-md class="editor-container" />
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/develop/Studio.vue
@@ -0,0 +1,65 @@
+<script lang="ts" setup>
+const selectedValues = ref([])
+const options = [
+  {
+    label: "Drive My Car",
+    value: "song1",
+  },
+  {
+    label: "Norwegian Wood",
+    value: "song2",
+  },
+  {
+    label: "You Won't See",
+    value: "song3",
+  },
+  {
+    label: "Nowhere Man",
+    value: "song4",
+  },
+  {
+    label: "Think For Yourself",
+    value: "song5",
+  },
+  {
+    label: "The Word",
+    value: "song6",
+  },
+  {
+    label: "Michelle",
+    value: "song7",
+  },
+  {
+    label: "What goes on",
+    value: "song8",
+  },
+  {
+    label: "Girl",
+    value: "song9",
+  },
+  {
+    label: "I'm looking through you",
+    value: "song10",
+  },
+  {
+    label: "In My Life",
+    value: "song11",
+  },
+  {
+    label: "Wait",
+    value: "song12",
+  },
+]
+const data = ref("")
+</script>
+
+<template>
+  <n-select
+    v-model:value="selectedValues"
+    multiple
+    filterable
+    placeholder="Select templates"
+    :options="options"
+  />
+  <AsyncEditor v-model:value="data" language="json" />
+</template>
